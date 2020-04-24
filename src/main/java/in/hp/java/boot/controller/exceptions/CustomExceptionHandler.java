4ec173569e08ed4c6694577f569c6593bc74e340
Hariprasath <balaprasad24@gmail.com>
2020-04-24 23:57:27 +0530

    Added custom exception handler as an advice for all controllers

---
+++ src/main/java/in/hp/java/boot/controller/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,59 @@
+package in.hp.java.boot.controller.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+/**
+ * Custom Exception Handler
+ * Used to impose org level exception standard
+ *
+ * @RestController - this acts as a controller as it directly sends response
+ * @ControllerAdvice
+ *      - the methods in the class should be common to all controllers,
+ *      - hence aop kind of advice is used
+ *
+ * Any exceptions returned from any controller comes to this handler
+ * The method is executed based on @ExceptionHandler advice
+ */
+@RestController
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * The below method is used to handle all the exceptions reported by controller
+     * It returns back custom exception with status code 500
+     *
+     * request.getDescription(false) - here false is passed to not reveal client info
+     *
+     * @param ex
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        GenericException genericException = new GenericException(
+                new Date().toString(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(genericException, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * This method is executed for only ResourceNotFoundException
+     *
+     * @param ex
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<Object> handleAllException(ResourceNotFoundException ex, WebRequest request) {
+        GenericException genericException = new GenericException(
+                new Date().toString(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(genericException, HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/in/hp/java/boot/controller/exceptions/ResourceNotFoundException.java
+++ src/main/java/in/hp/java/boot/controller/exceptions/ResourceNotFoundException.java
@@ -3,6 +3,11 @@ package in.hp.java.boot.controller.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception
+ * @ResponseStatus(HttpStatus.NOT_FOUND)
+ *      - throws spring format exception but with the given error code
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
