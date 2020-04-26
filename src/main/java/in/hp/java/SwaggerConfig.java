eccadc018fb359be833bfda0b0dbeb1ed76e1cf8
Hariprasath <balaprasad24@gmail.com>
2020-04-26 22:42:39 +0530

    Configured Swagger using Java Brains

--- src/main/java/in/hp/java/SwaggerConfig.java
+++ src/main/java/in/hp/java/SwaggerConfig.java
@@ -2,6 +2,7 @@ package in.hp.java;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
@@ -9,7 +10,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -33,9 +37,22 @@ public class SwaggerConfig {
     private static final Set<String> DEFAULT_PRODUCES_CONSUMES =
             new HashSet<>(Arrays.asList("application/json", "application.xml"));
 
+    /**
+     * Configuring and building Docket Object
+     * select() - allows to build the Docket with ApiSelectBuilder
+     * apis() - allows to configure the basePackage from which the models will be read
+     * paths() - allows to configure URI/resource path
+     * build() - returns a Docket object
+     *
+     * @return
+     */
     @Bean
     public Docket configureDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("in.hp.java"))
+//                .paths(PathSelectors.ant("/*"))
+                .build() // building Docker Object
                 .apiInfo(DEFAULT_API_INFO)
                 .produces(DEFAULT_PRODUCES_CONSUMES)
                 .consumes(DEFAULT_PRODUCES_CONSUMES);
