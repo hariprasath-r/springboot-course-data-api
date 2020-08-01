cd0302041db9dd8795a54bb13d547ea5a8744b92
Hariprasath <balaprasad24@gmail.com>
2020-08-02 00:42:23 +0530

    Spring Integration Test using @SpringBootTest

--- src/test/java/in/hp/java/CourseDataApiApplicationTests.java
+++ src/test/java/in/hp/java/CourseDataApiApplicationTests.java
@@ -1,16 +1,34 @@
 package in.hp.java;
 
+import in.hp.java.boot.topic.Topic;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CourseDataApiApplicationTests {
+
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
+		ResponseEntity<Topic[]> response = restTemplate.getForEntity("/topics", Topic[].class);
+
+		assertEquals(200, response.getStatusCodeValue());
+		assertEquals(0, response.getBody().length);
 	}
 
 }
