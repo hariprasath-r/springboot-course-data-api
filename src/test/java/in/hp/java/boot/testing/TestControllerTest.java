1b0c1de0f1f0e0c98b88c3bf78a0942cbdd3927b
Hariprasath <balaprasad24@gmail.com>
2020-08-01 17:38:09 +0530

    Using andExpect with MockMvcResultMatchers

--- src/test/java/in/hp/java/boot/testing/TestControllerTest.java
+++ src/test/java/in/hp/java/boot/testing/TestControllerTest.java
@@ -8,14 +8,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.junit.Assert.*;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 /**
  * @WebMvcTest will inject the dependency of specified controller class to the MockMvc
+ * It basically launches the spring context with only the mentioned controller
  */
 @RunWith(SpringRunner.class)
 @WebMvcTest(TestController.class)
@@ -29,13 +28,25 @@ public class TestControllerTest {
 
     @Test
     public void helloMockito() throws Exception {
-
+        // Preparing the request with payload, uri and response type
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .request(HttpMethod.GET, "/hello-mockito")
                 .accept(MediaType.APPLICATION_JSON);
 
-        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        /**
+         * mockMvc.perform will make the request call
+         * andExpect - we can pass matchers to check status, content and other things
+         * andExpect - we can also match the resulting json
+         */
+        mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello Mockito"))
+                .andReturn();
 
-        assertEquals("Hello Mockito", mvcResult.getResponse().getContentAsString());
+        /**
+         * asserting the response to verify
+         * assert is not required if the response is simple like a string, but we need it for complex scenario
+         */
+//        assertEquals("Hello Mockito", mvcResult.getResponse().getContentAsString());
     }
 }
